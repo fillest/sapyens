ce874f8a1f2e948d28b78aefafe400d5517fa218
Philipp <fsfeel@gmail.com>
2012-07-25 03:05:00 +0400

    add option to write migration records without applying their content

--- src/sapyens/migrate.py
+++ src/sapyens/migrate.py
@@ -36,13 +36,14 @@ def make_script_entry_point (migrations_dir, migrations_table_name = 'migrations
 		avail_mirgations = set(os.path.splitext(os.path.basename(p))[0] for p in glob.glob(migrations_dir + '/*.sql'))
 		pending_migrations = sorted(avail_mirgations - applied_migrations)
 
-		_apply_migrations(pending_migrations, migrations_dir, migrations_table_name, db_session, log)
+		_apply_migrations(pending_migrations, migrations_dir, migrations_table_name, db_session, log, opts.force_write)
 
 	return main
 
 def _parse_opts ():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('config', help = "config file")
+	parser.add_argument('-fw', '--force-write', nargs = '*', help = "write migration records without applying their content", default = [])
 	return parser.parse_args()
 
 def _get_applied_migrations (migrations_table_name, db_session, log):
@@ -60,14 +61,26 @@ def _get_applied_migrations (migrations_table_name, db_session, log):
 			raise
 	return applied_migrations
 
-def _apply_migrations (migrations, migrations_dir, migrations_table_name, db_session, log):
+def _apply_migrations (migrations, migrations_dir, migrations_table_name, db_session, log, migrations_to_force_write):
 	if migrations:
 		for migration_id in migrations:
-			fpath = migrations_dir + '/%s.sql' % migration_id
+			fpath = _migration_path(migrations_dir, migration_id)
 			log.info("Applying " + fpath)
 			with open(fpath, 'rb') as f:
-				db_session.execute(f.read())
+				if migration_id not in _paths_to_ids(migrations_to_force_write, migrations_dir):
+					db_session.execute(f.read())
 				db_session.execute('INSERT INTO %s (id) VALUES (:id)' % migrations_table_name, {
 					'id': migration_id,
 				})
 		db_session.commit()
+
+def _paths_to_ids (paths, migrations_dir):
+	for path in paths:
+		migration_id, _ext = os.path.splitext(os.path.basename(path))
+		p = _migration_path(migrations_dir, migration_id)
+		if not os.path.isfile(p):
+			raise ValueError(path)
+		yield migration_id
+
+def _migration_path (dir_path, migration_id):
+	return dir_path + '/%s.sql' % migration_id
