b9fc61380a22860fea2a656d993b5522de773d56
Philipp <fsfeel@gmail.com>
2012-09-23 02:40:59 +0400

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = [
 
 setuptools.setup(
 	name = 'sapyens',
-	version = '0.1.3',
+	version = '0.1.5',
 	description = 'SQLAlchemy & Pyramid enhancements',
 	long_description = 'SQLAlchemy & Pyramid enhancements',
 	author = 'Philipp Saveliev',
