bb1cb42ad4dac251d77516d76e1b30d57a82134b
Philipp <fsfeel@gmail.com>
2013-03-25 04:41:07 +0400

    .db - use sqlalchemy 0.8 (DeferredReflection)

--- src/sapyens/db.py
+++ src/sapyens/db.py
@@ -1,10 +1,8 @@
 import logging
 from contextlib import contextmanager
 import collections
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker, mapper
-from sqlalchemy import Table
-from sqlalchemy.orm.util import _is_mapped_class
+from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
+from sqlalchemy.orm import scoped_session, sessionmaker
 #NOTE conditional zope.sqlalchemy import in make_classes() below
 #NOTE pyramid.httpexceptions.HTTPNotFound import in notfound_tween_factory() below
 
@@ -21,63 +19,12 @@ def engine_log_level (level):
 		db_logger.level = old_level
 
 
-#TODO will be obsolete in sqla0.8 (see DeferredReflection)
-#see http://docs.sqlalchemy.org/en/rel_0_7/orm/examples.html#declarative-reflection
-#see http://hg.sqlalchemy.org/sqlalchemy/file/8e1b23314c39/examples/declarative_reflection/declarative_reflection.py
-#see https://bitbucket.org/zzzeek/sqlalchemy/pull-request/1/copied-declarativereflectedbase-from-the
-class DeclarativeReflectedBase(object):
-    _mapper_args = []
-
-    @classmethod
-    def __mapper_cls__(cls, *args, **kw):
-        """Declarative will use this function in lieu of
-        calling mapper() directly.
-
-        Collect each series of arguments and invoke
-        them when prepare() is called.
-        """
-
-        cls._mapper_args.append((args, kw))
-
-    @classmethod
-    def prepare(cls, engine):
-        """Reflect all the tables and map !"""
-        while cls._mapper_args:
-            args, kw  = cls._mapper_args.pop()
-            klass = args[0]
-            # autoload Table, which is already
-            # present in the metadata.  This
-            # will fill in db-loaded columns
-            # into the existing Table object.
-            if args[1] is not None:
-                table = args[1]
-                Table(table.name,
-                    cls.metadata,
-                    extend_existing=True,
-                    autoload_replace=False,
-                    autoload=True,
-                    autoload_with=engine,
-                    schema=table.schema)
-
-            # see if we need 'inherits' in the
-            # mapper args.  Declarative will have
-            # skipped this since mappings weren't
-            # available yet.
-            for c in klass.__bases__:
-                if _is_mapped_class(c):
-                    kw['inherits'] = c
-                    break
-
-            klass.__mapper__ = mapper(*args, **kw)
-
-
 class NotFound (Exception):
 	pass
 
-#DBObject = declarative_base()
-#DBObject = declarative_base(cls = DeclarativeReflectedBase)
-#class Reflected (DeclarativeReflectedBase, DBObject):
-class Reflected (DeclarativeReflectedBase, declarative_base()):
+Base = declarative_base(cls = DeferredReflection)
+
+class Reflected (Base):
 	__abstract__ = True
 
 	def __repr__ (self):
