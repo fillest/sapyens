5cb4bd63015020d39dcb0766bb7ffd13a4bd3664
Philipp <fsfeel@gmail.com>
2013-03-25 04:49:51 +0400

    increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = [
 
 setuptools.setup(
 	name = 'sapyens',
-	version = '0.1.7',
+	version = '0.1.8',
 	description = 'SQLAlchemy & Pyramid enhancements',
 	long_description = 'SQLAlchemy & Pyramid enhancements',
 	author = 'Philipp Saveliev',
