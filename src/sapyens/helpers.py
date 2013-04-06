139f10e006e53a63e6ed76300c736ef4083b47a8
Philipp <fsfeel@gmail.com>
2013-04-06 23:35:02 +0400

    .helpers.route_view_config

--- src/sapyens/helpers.py
+++ src/sapyens/helpers.py
@@ -1,7 +1,7 @@
-import venusian
-from pyramid.httpexceptions import HTTPNotFound
 import os
 import time
+import venusian
+from pyramid.httpexceptions import HTTPNotFound
 
 
 class add_route (object):
@@ -10,6 +10,22 @@ class add_route (object):
 
 	def register(self, scanner, name, wrapped):
 		scanner.config.add_route(*self._params)
+
+	def __call__(self, wrapped):
+		venusian.attach(wrapped, self.register)
+		return wrapped
+
+#TODO how to just combine decorators?
+class route_view_config (object):
+	def __init__(self, path, route_name, **view_config_params):
+		self._path = path
+		self._route_name = route_name
+		self._view_config_params = view_config_params
+
+	def register(self, scanner, name, wrapped):
+		scanner.config.add_route(self._route_name, self._path)
+
+		scanner.config.add_view(wrapped, route_name = self._route_name, **self._view_config_params)
 
 	def __call__(self, wrapped):
 		venusian.attach(wrapped, self.register)
