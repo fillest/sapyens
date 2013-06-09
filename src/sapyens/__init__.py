94c0d85cf959b6689cae763ef989f2ff9a454f9a
Philipp Saveliev <fsfeel@gmail.com>
2013-06-09 04:45:58 +0400

    cache_max_age argument for add_static_route

--- src/sapyens/__init__.py
+++ src/sapyens/__init__.py
@@ -1,2 +1,2 @@
-def add_static_route (config):
-	config.add_static_view('sapyens.static', 'sapyens:static/', cache_max_age = 3600)
+def add_static_route (config, cache_max_age = 3600):
+	config.add_static_view('sapyens.static', 'sapyens:static/', cache_max_age = cache_max_age)
