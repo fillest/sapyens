527dab62cdbd8d806fed53bcd80b4a67d21abbf7
Philipp Saveliev <fsfeel@gmail.com>
2014-09-19 13:42:12 +0400

    (breaks compat) change initialization, update to pyramid 1.5

--- src/sapyens/__init__.py
+++ src/sapyens/__init__.py
@@ -1,23 +1,23 @@
 from pyramid.security import authenticated_userid, has_permission
 import pyramid.tweens
+from pyramid.settings import asbool
 
 
-def include (config,
-		static_view = True,
-		has_permission_prop = True,
-		db_not_found_tween = True,
-):
-	if static_view:
-		params = {} if static_view is True else static_view
-		add_static_view(config, **params)
+def includeme (config):
+	settings = config.get_settings()
 
-	if has_permission_prop:
-		config.set_request_property(
-			lambda request: lambda permission: has_permission(permission, request.root, request),
-			'has_permission'
-		)
+	#TODO maybe better turn off by default?
+	if asbool(settings.get('sapyens.add_static_view', True)):
+		#TODO configurable attrs
+		add_static_view(config)
 
-	if db_not_found_tween:
+	# if has_permission_prop:
+	# 	config.set_request_property(
+	# 		lambda request: lambda permission: has_permission(permission, request.root, request),
+	# 		'has_permission'
+	# 	)
+
+	if asbool(settings.get('sapyens.db_not_found_tween', True)):
 		config.add_tween('sapyens.db.notfound_tween_factory', under = pyramid.tweens.EXCVIEW)
 
 def add_static_view (config, cache_max_age = 3600):
